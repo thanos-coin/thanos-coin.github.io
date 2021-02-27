19c120ff2adf2b9446f1989a49fbf3b2b630d3ef
ThanosDev <Thanos@mail.com>
2021-02-27 12:10:29 -0800

    Redo

---
+++ js/coming-soon.js
@@ -0,0 +1,10 @@
+(function($) {
+    "use strict"; // Start of use strict
+    $("#countdown").countdowntimer({
+        dateAndTime: "2021/03/05 17:00:00",
+        size: "lg",
+        displayFormat: "DHMS",
+        timeZone: -8
+    });
+
+})(jQuery); // End of use strict
