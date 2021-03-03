d3bb98394f3b405f55acd87bf4b02e7422bab389
ThanosDev <Thanos@mail.com>
2021-03-02 21:07:41 -0800

    Add presale

--- js/coming-soon.js
+++ js/coming-soon.js
@@ -1,7 +1,7 @@
 (function($) {
     "use strict"; // Start of use strict
     $("#countdown").countdowntimer({
-        dateAndTime: "2021/03/03 17:00:00",
+        dateAndTime: "2021/03/09 21:00:00",
         size: "lg",
         displayFormat: "DHMS",
         timeZone: -8
